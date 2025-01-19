9bbe18eefe4703b0334022d9e1e5c7fc0b817cc0
rojin-0 <156826271+rojin-0@users.noreply.github.com>
2025-01-19 23:06:03 +0545

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,21 @@
+package workshop7;
+
+public class Student {
+	String name;
+	int age;
+	Student(){
+		
+	}
+	public String getName(){
+		return name;
+	}
+	public int getAge(){
+		return age;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+}
